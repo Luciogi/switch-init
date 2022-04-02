8ffb1400af373e37d49f7bd8cf85691376817ce7
Luciogi <84625092+Luciogi@users.noreply.github.com>
2022-04-02 23:18:10 +0500

    Update runit2openrc.sh

--- runit2openrc.sh
+++ runit2openrc.sh
@@ -1,12 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Switching Runit --> Openrc"
-echo "Note: Make sure to stop Display Manager i.e sddm,gdm,lightdm"
-echo "Creating List of installed init services"
-pacman -Qsq runit > services.list
-echo "Downloading Openrc & Openrc services"
-pacman -Sw $(sed -e 's/runit/openrc/g' -e '/openrc-rc/d' -e '/rsm/d'< services.list)
-echo "Removing Runit"
-pacman -Rdd $(cat services.list)
-echo "Installing Openrc & Openrc services"
-pacman -S $(sed -e 's/runit/openrc/g' -e '/openrc-rc/d' -e '/rsm/d' < services.list)
+# https://www.coolgenerator.com/ascii-text-generator
+# RUNIT TO OPENRC
+echo ' __             __    __          __  __
+|__)    _ .|_    _)  /  \ _  _ _ |__)/
+| \ |_|| )||_   /__  \__/|_)(-| )| \ \__
+                         |               '
+
+# Create files
+touch openrc-list  openrc-list-1 runit-list-1
+
+echo "Creating List of installed Runit pkgs"
+pacman -Qsq runit > runit-list
+
+echo "Create List of required Openrc pkgs"
+sed -e 's/runit/openrc/g' -e '/rsm/d' -e '/openrc-rc/d' runit-list >> openrc-list
+
+# pacman -Sw $(cat openrc-list)
+pacman -Sw $(cat openrc-list) --noconfirm >> openrc-list-1 2>&1
+
+# Error check
+if grep "error" openrc-list-1;
+    then
+        echo "error"
+        # Create list of unavailable pkgs
+        sed -i -e 's/error: target not found://g' -e 's/ //g' openrc-list-1
+        # TODO remove unavailable pkgs
+        comm -23 <(sort openrc-list) <(sort openrc-list-1) > openrc-list-2
+        echo "Downloading OpenRc pkgs"
+        pacman -Sw $(cat openrc-list-2)
+        pacman -Rdd $(cat runit-list)
+        pacman -S $(cat openrc-list-2)
+        exit 1
+else
+    pacman -Rdd $(cat runit-list)
+    pacman -S $(cat openrc-list)
+fi
